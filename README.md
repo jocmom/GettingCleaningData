2389698a9d73421af1c91bd20afe7b92d4d4846e
pennyheadaway <jombi@web.de>
2014-10-22 10:32:02 +0200

    first working solution

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-GettingCleaningData
-===================
+Coursera - Getting and Cleaning Data: Course Project
+====================================================
+
+Introduction
+------------
+This repository contains my course project for the Coursera course 
+"Getting and Cleaning data". It contains the following files/folders
+
+- "run_analysis.r" script file - performing some Getting and Cleaning Data skills
+- "tidyData.txt" file - script result
+- "codebook.md" file - describing the variables, the data, and transformations 
+
+Requirements
+------------
+- Download UCI HAR Dataset 
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+- Extract the dataset to the folder "Dataset". This folder must be located in 
+the same folder as the "run_analysis.r" script
+
+
+The dataset
+-----------
+The dataset represent data collected from the accelerometers from the Samsung 
+Galaxy S smartphone. A full description is available at the site where the data 
+was obtained:
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+It contains several txt files which are further described in the downloaded 
+"Readme.txt"
+
+
+The script
+----------
+The "run_analysis.R" script does the following steps:
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each 
+measurement. 
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names. 
+5. From the data set in step 4, creates a second, independent tidy data set with
+the average of each variable for each activity and each subject.
+
+
+The CodeBook
+------------
+The CodeBook.md file explains the transformations performed and the resulting 
+data and variables.
--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,80 @@
 # Getting and cleaning data course project
+run_analysis <- function() {
+    # use "dplyr" library for fast joins
+    library(dplyr)
+    library(tidyr)
+    ############################################################################
+    # get 561 features
+    features <<-read.table("./Dataset/features.txt", 
+                           header = FALSE, 
+                           col.names = c("idx", "label"), 
+                           stringsAsFactors = FALSE)
+    ############################################################################    
+    # get 6 activities     
+    activities <<- read.table("./Dataset/activity_labels.txt",
+                              header = FALSE, 
+                              col.names = c("id", "label"), 
+                              stringsAsFactors = FALSE)
+    # cleanup feature names, remove "-", "(", ")" characters
+    features$label <<- gsub("\\(|\\)", "", features$label)
+    features$label <<- gsub(",|-", "_", features$label)
+    ############################################################################
+    # get test and training sensor values and step4 is easy to implement with
+    # the "col.names" parameter of read.table()
+    # step4: appropriately labels the data set with descriptive variable names.
+    testX <-read.table("./Dataset/test/X_test.txt", 
+                       header = FALSE, 
+                       col.names = features$label,
+                       colClasses = "numeric")
+    
+    trainX <-read.table("./Dataset/train/X_train.txt", 
+                        header = FALSE, 
+                        col.names = features$label,
+                        colClasses = "numeric")
+    # step1: merge test, train rows
+    tableX <- rbind_list(testX, trainX)
+    # step2: extract only the measurements on the mean and standard deviation
+    isStdOrMeanMeasurement <- names(tableX) %in% grep("mean|std", 
+                                                      names(tableX), 
+                                                      value = TRUE)
+    tableX <- tableX[, isStdOrMeanMeasurement]
+    ############################################################################
+    #get corresponding activities
+    testActivities <- read.table("./Dataset/test/y_test.txt",
+                                 header = FALSE, 
+                                 col.names = c("id"))
+    testActivities <- inner_join(testActivities, activities, by = "id")
+    trainActivities <- read.table("./Dataset/train/y_train.txt",
+                                  header = FALSE, 
+                                  col.names = c("id"))    
+    trainActivities <- inner_join(trainActivities, activities, by = "id")
+    # merge test, train activity rows
+    tableActivities <<- rbind(testActivities, trainActivities)
+    # step3: descriptive activity names to name the activities in the data set
+    tableX$activity <- tableActivities$label
+    ############################################################################
+    # get corresponding subjects
+    testSubjects <- read.table("./Dataset/test/subject_test.txt",
+                               header = FALSE, 
+                               col.names = c("subject"))
+    trainSubjects <- read.table("./Dataset/train/subject_train.txt",
+                                header = FALSE, 
+                                col.names = c("subject")) 
+    # merge test,train subject rows
+    tableSubjects <<- rbind(testSubjects, trainSubjects)
+    # add column to table
+    tableX$subject <- tableSubjects$subject
+    ############################################################################
+    # step5: From the data set in step 4, creates a second, independent tidy
+    # data set with the average of each variable for each activity and each 
+    # subject.
+    avgTable <<- 
+        tableX %>%
+        group_by(activity, subject) %>%
+        summarise_each(funs(mean))
+    # save tidy data
+    write.table(avgTable, file="tidyData.txt", row.names = FALSE)
+    ############################################################################
+    tableX
+                          
+}
